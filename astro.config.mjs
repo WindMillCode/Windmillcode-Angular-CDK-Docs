723ecc9808db26337ce19349baefbe72f5319024
windmillcode0 <michaelodumosu57@gmail.com>
2024-08-01 18:40:33 -0400

    [UPDATE] additional work

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site:"https://windmillcode.github.io/Windmillcode-Angular-CDK-Docs",
+  base:"/docs",
   outDir: 'docs',
   integrations: [
   starlight({
