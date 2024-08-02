f82d02ee9d0ae3212f64ea55b4ccc55970d0c6a4
windmillcode0 <michaelodumosu57@gmail.com>
2024-08-01 20:30:09 -0400

    [UPDATE] additional work

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,6 @@ export default defineConfig({
       // {
       //   label: 'Guides',
       //   items: [
-      //   // Each item here is one entry in the navigation menu.
       //   {
       //     label: 'Example Guide',
       //     slug: 'guides/example'
